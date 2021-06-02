5fe04858dfed20aa10ce9b43cae6adb709932252
kristina <christychri0@gmail.com>
2021-06-02 18:09:15 +0700

    refactored code, enabled footer links
    now both header & footer links are sync

--- src/App.vue
+++ src/App.vue
@@ -21,13 +21,14 @@
       >
         <v-btn
             v-for="link in links"
-            :key="link"
+            :key="`${link.label}-footer-link`"
             color="white"
             text
             rounded
             class="my-2"
+            :to="link.url"
         >
-          {{ link }}
+          {{ link.label }}
         </v-btn>
         <v-col
             class="primary lighten-2 py-4 text-center white--text"
