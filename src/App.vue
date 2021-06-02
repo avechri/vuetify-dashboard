903632a898192f3e43ba500ea3050fc69f4ccc2d
kristina <christychri0@gmail.com>
2021-06-02 15:03:11 +0700

    added a footer component + stylised Nav bar buttons

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
     <v-app-bar app color="primary" dark>
       <v-tool-bar-title>Vuetify Dashboard</v-tool-bar-title>
       <v-spacer></v-spacer>
-      <v-btn>Home</v-btn>
-      <v-btn>Login</v-btn>
+      <v-btn text rounded>Home</v-btn>
+      <v-btn text rounded>Login</v-btn>
     </v-app-bar>
     <!--Login Module-->
     <v-content>
@@ -35,6 +35,33 @@
       </v-card-actions>
     </v-card>
     </v-content>
+    <!--Footer Module-->
+      <v-footer
+          color="primary lighten-1"
+          padless
+      >
+        <v-row
+            justify="center"
+            no-gutters
+        >
+          <v-btn
+              v-for="link in links"
+              :key="link"
+              color="white"
+              text
+              rounded
+              class="my-2"
+          >
+            {{ link }}
+          </v-btn>
+          <v-col
+              class="primary lighten-2 py-4 text-center white--text"
+              cols="12"
+          >
+            {{ new Date().getFullYear() }} <strong>Vuetify Dashboard</strong>
+          </v-col>
+        </v-row>
+      </v-footer>
   </v-app>
 </template>
 
@@ -42,7 +69,11 @@
 
 export default {
   data: () => ({
-    showPassword: false
+    showPassword: false,
+    links: [
+        'Home',
+        'Login'
+    ]
   }),
 };
 </script>
