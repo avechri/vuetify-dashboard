74fdbe190627b8974296e20860517e3da04601a4
kristina <christychri0@gmail.com>
2021-06-02 14:17:53 +0700

    added styles to elements

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,20 @@
       </v-card-title>
       <v-card-text>
         <v-form>
-          <v-text-field label="Username"/>
-          <v-text-field type="password" label="Password"/>
+          <v-text-field
+              prepend-icon="mdi-account-circle"
+              label="Username"/>
+          <v-text-field
+              type="password"
+              label="Password"
+              prepend-icon="mdi-lock"
+              append-icon="mdi-eye-off"
+          />
         </v-form>
       </v-card-text>
       <v-card-actions>
-        <v-btn>Register</v-btn>
-        <v-btn>Login</v-btn>
+        <v-btn color="success">Register</v-btn>
+        <v-btn color="info">Login</v-btn>
       </v-card-actions>
     </v-card>
   </v-app>
