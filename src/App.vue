6317f97228650974e8144eeff644e4c31b97d62c
kristina <christychri0@gmail.com>
2021-06-02 14:20:58 +0700

    added space between elements

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,10 @@
           />
         </v-form>
       </v-card-text>
+      <v-divider></v-divider>
       <v-card-actions>
         <v-btn color="success">Register</v-btn>
+        <v-spacer></v-spacer>
         <v-btn color="info">Login</v-btn>
       </v-card-actions>
     </v-card>
