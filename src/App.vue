d991be3455112b785473a4da943cd1e94c516bf8
kristina <christychri0@gmail.com>
2021-06-02 17:45:45 +0700

    returned my version of the App file

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,82 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <!--Navigation Module-->
+    <v-app-bar app color="primary" dark>
+      <v-tool-bar-title>Vuetify Dashboard</v-tool-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn text rounded>Home</v-btn>
+      <v-btn text rounded>Login</v-btn>
+    </v-app-bar>
+    <!--Login Module-->
+    <v-content>
+      <v-card width="400" class="mx-auto mt-5">
+        <v-card-title>
+          <h1 class="display-1">Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+                prepend-icon="mdi-account-circle"
+                label="Username"/>
+            <v-text-field
+                :type="showPassword ? 'text' : 'password'"
+                label="Password"
+                prepend-icon="mdi-lock"
+                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="showPassword = !showPassword"
+            />
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="success">Register</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="info">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-content>
+    <!--Footer Module-->
+    <v-footer
+        color="primary lighten-1"
+        padless
+    >
+      <v-row
+          justify="center"
+          no-gutters
+      >
+        <v-btn
+            v-for="link in links"
+            :key="link"
+            color="white"
+            text
+            rounded
+            class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col
+            class="primary lighten-2 py-4 text-center white--text"
+            cols="12"
+        >
+          {{ new Date().getFullYear() }} <strong>Vuetify Dashboard</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-app>
 </template>
+
+<script>
+
+export default {
+  data: () => ({
+    showPassword: false,
+    links: [
+      'Home',
+      'Login'
+    ]
+  }),
+};
+</script>
 
 <style lang="scss">
 #app {
