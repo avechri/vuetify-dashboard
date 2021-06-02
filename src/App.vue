c09cc6527a96fca94a46b58da1a667df13e5232c
kristina <christychri0@gmail.com>
2021-06-02 18:10:53 +0700

    refactored header links

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,15 @@
     <v-app-bar app color="primary" dark>
       <v-tool-bar-title>Vuetify Dashboard</v-tool-bar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded to="/">Home</v-btn>
-      <v-btn text rounded to="/login">Login</v-btn>
+      <v-btn
+          v-for="link in links"
+          :key="`${link.label}-header-link`"
+          text
+          rounded
+          :to="link.url"
+      >
+        {{ link.label }}
+      </v-btn>
     </v-app-bar>
     <v-content>
       <router-view></router-view>
