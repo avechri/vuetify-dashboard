6326f1c149033012984b351608262649add0ee22
kristina <christychri0@gmail.com>
2021-06-03 11:48:22 +0700

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: ['plugin:prettier/recommended'],
+  plugins: [],
+  // add your custom rules here
+  rules: {},
+}
