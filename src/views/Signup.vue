7f225ce10f933882a8ff9f5f9e234a3fa35f6815
kristina <christychri0@gmail.com>
2021-06-08 19:26:27 +0700

    added a form with email (+ validation) and browser select

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,10 +1,35 @@
 <template>
-  <h1>Sign up</h1>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Sign up</h1>
+        <v-form>
+          <v-text-field
+            label="Email"
+            type="email"
+            v-model="email"
+            :rules="emailRules"
+            required
+          />
+          <v-autocomplete label="Which browser do you use?" :items="browsers" />
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'Signup'
+  data() {
+    return {
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ],
+      browsers: ['Chrome', 'Safari', 'Mozilla Firefox', 'Microsoft Edge']
+    }
+  }
 }
 </script>
 
