d10b5c7e1aca31408cc2c476d99aff76e01deb15
kristina <christychri0@gmail.com>
2021-06-08 17:14:44 +0700

    made a single row for all statistics & each takes 1/4 of all the row
    //
    
    grid system

--- src/components/StatisticCard.vue
+++ src/components/StatisticCard.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card>
     <v-card-title>{{ statistic.title }}</v-card-title>
-    <v-card-text class="display-2">{{ statistic.value }}</v-card-text>
+    <v-card-text class="display-2 text-left">{{ statistic.value }}</v-card-text>
   </v-card>
 </template>
 
