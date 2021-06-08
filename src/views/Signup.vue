b559411e1184945ff3d139c7e39e2172e700a02e
kristina <christychri0@gmail.com>
2021-06-08 21:02:20 +0700

    added a birthday date picker

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -12,10 +12,41 @@
             required
           />
           <v-autocomplete label="Which browser do you use?" :items="browsers" />
-          <v-file-input
-            accept="image/*"
-            label="Attach profile pic"
-          ></v-file-input>
+          <v-file-input accept="image/*" label="Attach profile pic" />
+
+          <v-menu
+            ref="menu"
+            v-model="menu"
+            :close-on-content-click="false"
+            :return-value.sync="birthdayDate"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="birthdayDate"
+                label="Picker in menu"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker v-model="birthdayDate" no-title scrollable>
+              <v-spacer></v-spacer>
+              <v-btn text color="primary" @click="menu = false">
+                Cancel
+              </v-btn>
+              <v-btn
+                text
+                color="primary"
+                @click="$refs.menu.save(birthdayDate)"
+              >
+                OK
+              </v-btn>
+            </v-date-picker>
+          </v-menu>
         </v-form>
       </v-col>
     </v-row>
@@ -31,7 +62,9 @@ export default {
         v => !!v || 'E-mail is required',
         v => /.+@.+/.test(v) || 'E-mail must be valid'
       ],
-      browsers: ['Chrome', 'Safari', 'Mozilla Firefox', 'Microsoft Edge']
+      browsers: ['Chrome', 'Safari', 'Mozilla Firefox', 'Microsoft Edge'],
+      birthdayDate: new Date().toISOString().substr(0, 10),
+      menu: false
     }
   }
 }
